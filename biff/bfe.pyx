9c8513344d70fbe1e80b87c3efca86724a571f2d
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-08-10 16:39:08 +0200

    compute density

--- biff/bfe.pyx
+++ biff/bfe.pyx
@@ -26,10 +26,37 @@ cdef extern from "src/bfe_helper.c":
 __all__ = ['density']
 
 cpdef density(double[:,::1] xyz,
-              double G, double M, double r_s,
+              double M, double r_s,
               double[:,:,::1] Anlm, int nmax, int lmax):
     """
     density()
+    """
+
+    cdef:
+        int i,n,l,m
+        int ncoords = xyz.shape[0]
+        double r,X,phi
+        double[::1] dens = np.zeros(ncoords)
+
+    for i in range(ncoords):
+        r = sqrt(xyz[i,0]*xyz[i,0] + xyz[i,1]*xyz[i,1] + xyz[i,2]*xyz[i,2])
+        X = xyz[i,2]/r # cos(theta)
+        phi = atan2(xyz[i,1], xyz[i,0])
+
+        for n in range(nmax+1):
+            for l in range(lmax+1):
+                for m in range(l+1):
+                    dens[i] += Anlm[n,l,m] * rho_nlm(r, phi, X, r_s, n, l, m)
+
+        dens[i] *= M/(r_s*r_s*r_s)
+
+    return np.array(dens)
+
+cpdef potential(double[:,::1] xyz,
+                double G, double M, double r_s,
+                double[:,:,::1] Anlm, int nmax, int lmax):
+    """
+    potential()
     """
 
     cdef:
@@ -51,21 +78,3 @@ cpdef density(double[:,::1] xyz,
         pot[i] *= G*M/r_s
 
     return np.array(pot)
-
-# cpdef apw_density(double[::1] xyz,
-#                   double G, double M, double r_s,
-#                   double[:,:,::1] Anlm, int nmax, int lmax):
-#     cdef:
-#         double r = sqrt(xyz[0]*xyz[0] + xyz[1]*xyz[1] + xyz[2]*xyz[2])
-#         double s = r / r_s
-#         double costheta = xyz[2]/r
-#         double phi = atan2(xyz[1], xyz[0])
-#         int n,l,m
-#         double dens = 0.
-
-#     for n in range(nmax+1):
-#         for l in range(lmax+1):
-#             for m in range(-l,l+1):
-#                 dens += Anlm[n,l,m] * rho_nlm(s, phi, costheta, n, l, m)
-
-#     return M/(4*M_PI*r_s*r_s*r_s) * dens
