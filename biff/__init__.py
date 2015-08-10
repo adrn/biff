3423a6a91200829cb36baf878d7b3a2ee01a5601
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-08-10 13:12:46 +0200

    added astropy init files

---
+++ biff/__init__.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+Basis function expansions of gravitational potentials.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    # from example_mod import *
+    pass
