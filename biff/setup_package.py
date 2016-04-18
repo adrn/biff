e857d4d2d177e628147fb2148cda13dd71b1c78d
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-18 17:01:15 -0400

    try removing cpotential source

--- biff/setup_package.py
+++ biff/setup_package.py
@@ -8,9 +8,9 @@ from astropy_helpers import setup_helpers
 def get_extensions():
 
     # Get gary path
-    import gary
-    gary_base_path = os.path.split(gary.__file__)[0]
-    gary_path = os.path.join(gary_base_path, 'potential')
+    # import gary
+    # gary_base_path = os.path.split(gary.__file__)[0]
+    # gary_path = os.path.join(gary_base_path, 'potential')
 
     coeff_cfg = setup_helpers.DistutilsExtensionArgs()
     coeff_cfg['include_dirs'].append('numpy')
@@ -28,7 +28,7 @@ def get_extensions():
     bfe_cfg['sources'].append('biff/bfe.pyx')
     bfe_cfg['sources'].append('biff/src/bfe.c')
     bfe_cfg['sources'].append('biff/src/bfe_helper.c')
-    bfe_cfg['sources'].append(os.path.join(gary_path, 'src', 'cpotential.c'))
+    # bfe_cfg['sources'].append(os.path.join(gary_path, 'src', 'cpotential.c'))
     bfe_cfg['libraries'] = ['gsl', 'gslcblas']
     bfe_cfg['extra_compile_args'] = ['--std=gnu99']
 
