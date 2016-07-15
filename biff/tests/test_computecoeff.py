d3e8280c5276a38a5cab9fc3a83a77e524e5fb6b
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-07-15 15:00:39 -0400

    WIP: adding test using flattened hernquist and comparing against mathematica

--- biff/tests/test_computecoeff.py
+++ biff/tests/test_computecoeff.py
@@ -4,13 +4,19 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
+import os
+
 # Third-party
 import astropy.units as u
+from astropy.utils.data import get_pkg_data_filename
 from astropy.constants import G as _G
 G = _G.decompose([u.kpc,u.Myr,u.Msun]).value
+import matplotlib as mpl
+import matplotlib.pyplot as pl
 import numpy as np
 import gala.potential as gp
 from gala.units import galactic
+from scipy.integrate import quad
 
 # Project
 from ..core import compute_coeffs
@@ -80,6 +86,19 @@ def test_plummer():
     bfe_pot = potential(xyz, Snlm, Tnlm, nmax, lmax, G, true_M, true_r_s)
     bfe_grad = gradient(xyz, Snlm, Tnlm, nmax, lmax, G, true_M, true_r_s)
 
+    # fig,axes = pl.subplots(3, 1, figsize=(6,12), sharex=True)
+
+    # axes[0].loglog(x, true_dens)
+    # axes[0].loglog(x, bfe_dens)
+
+    # axes[1].semilogx(x, true_pot)
+    # axes[1].semilogx(x, bfe_pot)
+
+    # axes[2].semilogx(x, true_grad[:,0])
+    # axes[2].semilogx(x, bfe_grad[:,0])
+
+    # pl.show()
+
     assert np.allclose(true_dens, bfe_dens, rtol=2E-3)
     assert np.allclose(true_pot, bfe_pot, rtol=1E-6)
     assert np.allclose(true_grad[:,0], bfe_grad[:,0], rtol=5E-3)
@@ -88,69 +107,131 @@ def test_plummer():
     # print(np.abs((bfe_grad[:,0] - true_grad[:,0]) / true_grad[:,0]).max())
 
 # ----------------------------------------------------------------------------
+# Non-spherical, axisymmetric
 
-def miyamoto_nagai_density(x, y, z, M, a, b):
-    R2 = x**2 + y**2
-    sqrt_zb = np.sqrt(z**2+b**2)
+def flattened_hernquist_density_s(s, M, a, q):
+    return M*a / (2*np.pi) / (s * (a + s)**3)
 
-    A = (b**2*M / (4*np.pi))
-    numer = a*R2 + (a + 2*sqrt_zb)*(a + sqrt_zb)**2
-    denom = (R2 + (a + sqrt_zb)**2)**2.5 * sqrt_zb**3
+def flattened_hernquist_density(x, y, z, M, a, q):
+    s = np.sqrt(x*x + y*y + z*z/(q*q))
+    return flattened_hernquist_density_s(s, M, a, q)
 
-    return A*numer/denom
+def _integrand_helper(tau, xyz, M, a, q):
+    x,y,z = xyz
+    m = a*np.sqrt((x*x + y*y) / (a*a + tau) + z*z / (q*q + tau))
+    return flattened_hernquist_density_s(m, M, a, q) / ((tau+a*a)*np.sqrt(tau+q*q))
 
-def test_miyamoto_nagai():
-    M = 1E10
+def integrand(tau, i, xyz, M, a, q):
+    if i in [0,1]:
+        denom = tau + a*a
+    elif i == 2:
+        denom = tau + q*q
+    else:
+        raise ValueError("WTF")
+
+    return _integrand_helper(tau, xyz, M, a, q) * xyz[i] / denom
+
+def flattened_hernquist_gradient(x, y, z, G, M, a, q):
+    A = 2*np.pi*G*a**2*q
+    gx = A * quad(integrand, 0, np.inf, args=(0, (x, y, z), M, a, q), limit=1000)[0]
+    gy = A * quad(integrand, 0, np.inf, args=(1, (x, y, z), M, a, q))[0]
+    gz = A * quad(integrand, 0, np.inf, args=(2, (x, y, z), M, a, q))[0]
+
+    return np.array([gx, gy, gz])
+
+def test_flattened_hernquist():
+    G = 1.
+    M = 1
     a = 1.
-    b = 1.
+    q = 0.9
 
-    x = np.logspace(-2,1,512)
-    xyz = np.zeros((len(x),3))
-    xyz[:,0] = x
+    nmax = 4
+    lmax = 6
 
-    pot = gp.MiyamotoNagaiPotential(m=M, a=a, b=b, units=galactic)
-    true_pot = pot.value(xyz.T).value
-    true_dens = pot.density(xyz.T).value
-    true_grad = pot.gradient(xyz.T).value
+    # x = np.logspace(-2,1,512)
+    # xyz = np.zeros((len(x),3))
+    # xyz[:,0] = x
 
-    nmax = 16
-    lmax = 0
+    # TODO: no potential class for this
+    # pot = gp.MiyamotoNagaiPotential(m=M, a=a, b=b, units=galactic)
+    # true_pot = pot.value(xyz.T).value
+    # true_dens = pot.density(xyz.T).value # TODO: until fixed in Gala
+    # true_grad = pot.gradient(xyz.T).value.T
 
-    Snlm = np.zeros((nmax+1,lmax+1,lmax+1))
-    Snlm_e = np.zeros((nmax+1,lmax+1,lmax+1))
-    Tnlm = np.zeros((nmax+1,lmax+1,lmax+1))
-    Tnlm_e = np.zeros((nmax+1,lmax+1,lmax+1))
+    Snlm = np.zeros((nmax+1,lmax+1,lmax+1)) + np.nan
+    Snlm_e = np.zeros((nmax+1,lmax+1,lmax+1)) + np.nan
+    Tnlm = np.zeros((nmax+1,lmax+1,lmax+1)) + np.nan
+    Tnlm_e = np.zeros((nmax+1,lmax+1,lmax+1)) + np.nan
     for n in range(nmax+1):
-        for l in range(lmax+1):
-            for m in range(l+1):
-                nlm = [n,l,m]
-                (S,Serr),(T,Terr) = compute_coeffs(miyamoto_nagai_density,
-                                                   nlm=nlm,
-                                                   M=M, r_s=a,
-                                                   args=(M,a,b))
-                Snlm[n,l,m] = S
-                Snlm_e[n,l,m] = Serr
-                Tnlm[n,l,m] = T
-                Tnlm_e[n,l,m] = Terr
+        for l in range(0,lmax+1,2): # only even l
+            # Ignoring m because m > 0 not relevant for axisymmetric
+            m = 0
+            print(n,l,m)
+            nlm = [n,l,m]
+            (S,Serr),(T,Terr) = compute_coeffs(flattened_hernquist_density,
+                                               nlm=nlm,
+                                               M=M, r_s=a,
+                                               args=(M,a,q))
+            Snlm[n,l,m] = S
+            Snlm_e[n,l,m] = Serr
+            Tnlm[n,l,m] = T
+            Tnlm_e[n,l,m] = Terr
 
-    # Snlm[(np.abs(Snlm) < np.abs(Snlm_e))] = 0.
-    # Tnlm[(np.abs(Tnlm) < np.abs(Tnlm_e))] = 0.
+    coeff_path = os.path.abspath(get_pkg_data_filename('../data/Snlm-mathematica.csv'))
+    m_Snl0 = np.loadtxt(coeff_path, delimiter=',')
+    m_Snl0 = m_Snl0[:,::2] # every other l
 
-    bfe_dens = density(xyz, Snlm, Tnlm, nmax, lmax, M, a)
-    bfe_pot = potential(xyz, Snlm, Tnlm, nmax, lmax, G, M, a)
-    bfe_grad = gradient(xyz, Snlm, Tnlm, nmax, lmax, G, M, a)
+    print("python:", Snlm[0,::2,0])
+    print("mathematica:", m_Snl0[0])
+    return
 
-    import matplotlib.pyplot as pl
+    fig,ax = pl.subplots(1,1,figsize=(10,8))
+    n,l = np.mgrid[:nmax+1, :lmax+1]
+    c = ax.scatter(n.ravel(), l.ravel(), c=Snlm[:,:,0].ravel(), s=64,
+                   norm=mpl.colors.SymLogNorm(1E-5), cmap='RdBu_r',
+                   vmin=-100, vmax=100, linewidths=1., edgecolors='#666666')
 
-    fig,axes = pl.subplots(3, 1, figsize=(6,12), sharex=True)
+    ax.xaxis.set_ticks(np.arange(0,nmax+1,1))
+    ax.yaxis.set_ticks(np.arange(0,lmax+1,1))
 
-    axes[0].loglog(x, true_dens)
-    axes[0].loglog(x, bfe_dens)
+    ax.set_xlim(-0.5, nmax+0.5)
+    ax.set_ylim(-0.5, lmax+0.5)
 
-    axes[1].semilogx(x, true_pot)
-    axes[1].semilogx(x, bfe_pot)
+    ax.set_xlabel('$n$')
+    ax.set_ylabel('$l$')
 
-    axes[2].semilogx(x, true_grad[0])
-    axes[2].semilogx(x, bfe_grad[:,0])
+    tickloc = np.concatenate((-10.**np.arange(2,-5-1,-1),
+                              10.**np.arange(-5,2+1,1)))
+    fig.colorbar(c, ticks=tickloc, format='%.0e')
+    fig.tight_layout()
+    # pl.show()
+
+    Snlm[np.isnan(Snlm)] = 0.
+    Tnlm[np.isnan(Tnlm)] = 0.
+
+    # contour plot in r,θ at φ=0
+    fig,ax = pl.subplots(1, 1, figsize=(8,8))
+
+    rgrid = np.logspace(-1, 1., 128)
+    θgrid = np.linspace(0, np.pi, 128)
+
+    r,θ = np.meshgrid(rgrid,θgrid)
+    x = r*np.sin(θ)
+    z = r*np.cos(θ)
+
+    _xyz = np.vstack((x.ravel(),np.zeros_like(x.ravel()),z.ravel()))
+    bfe_dens = density(np.ascontiguousarray(_xyz.T), Snlm, Tnlm, nmax, lmax, M, a)
+    true_dens = flattened_hernquist_density(*_xyz, M, a, q)
+
+    levels = 10**np.linspace(-4.5, 1, 16)
+    ax.contour(np.log10(r), θ, true_dens.reshape(x.shape),
+               levels=levels, colors='k',
+               locator=mpl.ticker.LogLocator(), label='True')
+    ax.contour(np.log10(r), θ, bfe_dens.reshape(x.shape),
+               levels=levels, colors='r',
+               locator=mpl.ticker.LogLocator(), label='BFE')
+
+    ax.legend()
+    fig.tight_layout()
 
     pl.show()
